499d27fa4f39ba8fc23729261f50100fb0f02f8f
saaim <125155759+saaim12@users.noreply.github.com>
2025-08-01 14:55:54 +0500

    done jiinja template

--- app.py
+++ app.py
@@ -2,12 +2,30 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+# @app.route('/')
+# def home():
+#     #it will see in the /templates  folder for the html files
+#     return render_template("home_with_jinja.html")
+#
+#
+#Jinja is a template engine that lets us serve dynamic data to the template files.
+# Delimiters#
+# Let’s explore some delimiters used in Jinja Syntax.
+#
+# {% ... %} is used for statements.
+# {{ ... }} is used for variables.
+# {# ... #} is used for comments.
+# # ... ## is used for line statements.
+# this is how we pass variables
+users={
+                "Archie":"Amsterdam",
+                "Veronica":"London",
+                "Betty":"San Francisco",
+                "Jughead":"Los Angeles"
+            }
 @app.route('/')
 def home():
-    #it will see in the /templates  folder for the html files
-    return render_template("home_with_jinja.html")
-
-
+    return render_template('home_with_jinja.html',users=users)
 
 
 if __name__=='__main__':
