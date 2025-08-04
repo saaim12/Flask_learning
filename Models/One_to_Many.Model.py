f8d132376b1c319ad161f2d1b4cb8bb082a23838
saaim <125155759+saaim12@users.noreply.github.com>
2025-08-05 01:47:43 +0500

    flask learning

--- Models/One_to_Many.Model.py
+++ Models/One_to_Many.Model.py
@@ -7,15 +7,22 @@ class Employee(db.Model):
     age=db.Column(db.Integer,nullable=False)
     name=db.Column(db.String(100),nullable=False)
     department_id=db.Column(db.Integer,db.ForeignKey('department.id'),nullable=False)
-
+    is_head_of = db.Column(db.String, db.ForeignKey('department.id'), nullable=True)
 class Department(db.Model):
     id=db.Column(db.Integer,nullable=False,primary_key=True)
     name=db.Column(db.String(100),nullable=False)
     location = db.Column(db.String(120), nullable=False)
     employees = db.relationship('Employee',backref = 'department')
+    head = db.relationship('Employee', backref='head_of_department',uselist=False)
 
+
+#many to many relationship
+project_members = db.Table('project_members',
+    db.Column('employee_id', db.Integer, db.ForeignKey('employee.employee_id'), primary_key=True),
+    db.Column('project_id', db.Integer, db.ForeignKey('project.project_id'), primary_key=True)
+)
 
 class Project(db.Model):
     project_id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
-
+    members=db.relationship('Employee', secondary=project_members, backref='projects')
